73afad156f688ddd6e6cbf792df1f5343ae4380a
Ash Charles <ashcharles@gmail.com>
2013-12-09 21:05:50 +0100

    Add bootimage as dependency of uinitrd
    To ensure all the boot files are populated into the boot directory,
    run bootimage prior to making the uinitrd.

--- uinitrd/Android.mk
+++ uinitrd/Android.mk
@@ -1,6 +1,6 @@
 .phony: uinitrd
 
 # need uboot-mkimage installed...should really just get it from u-boot directory
-uinitrd: ramdisk
+uinitrd: bootimage ramdisk
 	@-mkdir -p $(PRODUCT_OUT)/boot
 	mkimage -A arm -O linux -T ramdisk -C gzip -d $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/boot/uInitrd
