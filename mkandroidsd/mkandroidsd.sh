3ddc70702ab2d2be461acd29df82cb208df175c6
Ash Charles <ashcharles@gmail.com>
2014-01-16 15:33:10 -0800

    Move sudo use inside mkandroidsd script
    The Android environment setup (envsetup.sh) applies only to the current
    user.  As such, it is necessary to specify the path to call the
    mkandroidsd script with sudo---it is no longer on the path. Instead,
    we move the calls to sudo inside the script.  The user can call it
    without sudo but will be prompted for sudo permissions.

--- mkandroidsd/mkandroidsd.sh
+++ mkandroidsd/mkandroidsd.sh
@@ -2,9 +2,9 @@
 # Based on the rowboat packaging script
 if [[ $# -ne 2 ]]; then
     echo "This utility creates a bootable microSD card for Android."
-    echo "Usage: $0 <sd-drive> <product-name>"
+    echo "Usage: `basename $0` <sd-drive> <product-name>"
     echo "Example:"
-    echo "	sudo out/host/linux-x86/bin/mkcard /dev/mmcblk0 overo"
+    echo "	`basename $0` /dev/mmcblk0 overo"
     exit -1
 fi
 
@@ -35,11 +35,11 @@ if ! [ $ans == 'y' ]; then
 fi
 
 echo "[Unmounting all existing partitions on the device ]"
-umount $1* &> /dev/null
+sudo umount $1* &> /dev/null
 
 echo "[Partitioning $1...]"
 DRIVE=$1
-dd if=/dev/zero of=$DRIVE bs=1024 count=1024 &>/dev/null
+sudo dd if=/dev/zero of=$DRIVE bs=1024 count=1024 &>/dev/null
 # 64MB VFAT boot partition, 1GB EXT4 system partition, 1GB EXT4 cache partition, remainder is EXT4 data partition
 # Sector size is 512-bytes
 {
@@ -47,52 +47,52 @@ echo 128,130944,0x0C,*
 echo 131072,2097152,,-
 echo 2228224,2097152,,-
 echo 4325376,,,-
-} | sfdisk --force -D -uS -H 255 -S 63 $DRIVE &> /dev/null
+} | sudo sfdisk --force -D -uS -H 255 -S 63 $DRIVE &> /dev/null
 
 echo "[Making boot partition...]"
 if [ -b ${1}1 ]; then
-    mkfs.vfat -F 32 -n boot "$1"1 &> /dev/null
-    mount "$1"1 /mnt
+    sudo mkfs.vfat -F 32 -n boot "$1"1 &> /dev/null
+    sudo mount "$1"1 /mnt
 else
-    mkfs.vfat -F 32 -n boot "$1"p1 &> /dev/null
-    mount "$1"p1 /mnt
+    sudo mkfs.vfat -F 32 -n boot "$1"p1 &> /dev/null
+    sudo mount "$1"p1 /mnt
 fi
-cp -v ${BOOTDIR}/* /mnt/
-umount /mnt
+sudo cp -v ${BOOTDIR}/* /mnt/
+sudo umount /mnt
 
 echo "[Making system partition...]"
 if [ -b ${1}2 ]; then
-    mkfs.ext4 -L system "$1"2 &> /dev/null
-    mount "$1"2 /mnt
+    sudo mkfs.ext4 -L system "$1"2 &> /dev/null
+    sudo mount "$1"2 /mnt
 else
-    mkfs.ext4 -L system "$1"p2 &> /dev/null
-    mount "$1"p2 /mnt
+    sudo mkfs.ext4 -L system "$1"p2 &> /dev/null
+    sudo mount "$1"p2 /mnt
 fi
-tar jxvf ${OUTDIR}/${SYSTEM} --strip-components=1 -C /mnt &> /dev/null
+sudo tar jxvf ${OUTDIR}/${SYSTEM} --strip-components=1 -C /mnt &> /dev/null
 sync
-umount /mnt
+sudo umount /mnt
 
 echo "[Making cache partition...]"
 if [ -b ${1}3 ]; then
-    mkfs.ext4 -L cache "$1"3 &> /dev/null
-    mount "$1"3 /mnt
+    sudo mkfs.ext4 -L cache "$1"3 &> /dev/null
+    sudo mount "$1"3 /mnt
 else
-    mkfs.ext4 -L cache "$1"p3 &> /dev/null
-    mount "$1"p3 /mnt
+    sudo mkfs.ext4 -L cache "$1"p3 &> /dev/null
+    sudo mount "$1"p3 /mnt
 fi
 sync
-umount /mnt
+sudo umount /mnt
 
 echo "[Making data partition]"
 if [ -b ${1}4 ]; then
-    mkfs.ext4 -L data "$1"4 &> /dev/null
-    mount "$1"4 /mnt
+    sudo mkfs.ext4 -L data "$1"4 &> /dev/null
+    sudo mount "$1"4 /mnt
 else
-    mkfs.ext4 -L data "$1"p4 &> /dev/null
-    mount "$1"p4 /mnt
+    sudo mkfs.ext4 -L data "$1"p4 &> /dev/null
+    sudo mount "$1"p4 /mnt
 fi
-tar jxvf ${OUTDIR}/${DATA} --strip-components=1 -C /mnt &> /dev/null
+sudo tar jxvf ${OUTDIR}/${DATA} --strip-components=1 -C /mnt &> /dev/null
 sync
-umount /mnt
+sudo umount /mnt
 
 echo "[Done]"
