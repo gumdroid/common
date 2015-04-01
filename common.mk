6b7b713e346de4d36fef162180be282fc0759c6e
Ash Charles <ashcharles@gmail.com>
2015-04-01 16:54:57 -0700

    Update 'gumstix' make target
    We no longer have a standalone boot-files target---these get rebuilt
    and pulled in by the droid target as needed.
    
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- common.mk
+++ common.mk
@@ -6,19 +6,7 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 
-.phony: gumstix bootfiles tarballs clean-tarballs
+.phony: gumstix
 
-# top-level make target
-gumstix: droid bootfiles tarballs
-
-tarballs: systemtarball userdatatarball
-
-clean-tarballs:
-	$(hide) -rm $(PRODUCT_OUT)/system.tar.bz2
-	$(hide) -rm $(PRODUCT_OUT)/userdata.tar.bz2
-
-# dependencies for the bootfiles target are filled in by
-# any packages that need to be included on the boot partition.
-# PRODUCT_PACKAGES doesn't work as these aren't including
-# the Android make classes.
-bootfiles:
+# top-level convenience make target
+gumstix: droid systemtarball userdatatarball
