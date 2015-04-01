f8f09fd189016ebec2b67f2fd06130b99a58abdb
Ash Charles <ashcharles@gmail.com>
2015-04-01 16:43:05 -0700

    Clean-up uinitrd creation
    Make sure it gets re-created when the ramdisk is updated.
    
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- uinitrd/Android.mk
+++ uinitrd/Android.mk
@@ -1,8 +1,21 @@
-.phony: uinitrd
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := uinitrd
+
+.phony: uinitrd clean-uinitrd
+
+UINITRD_TARGET := $(PRODUCT_OUT)/boot/uInitrd
+uinitrd: $(UINITRD_TARGET)
+
+clean-uinitrd:
+	$(hide) -rm $(UINITRD_TARGET)
 
 # need uboot-mkimage installed...should really just get it from u-boot directory
-uinitrd: ramdisk
+# from some unknown reason, the prerequisite can't be INSTALLED_RAMDISK_TARGET...
+$(UINITRD_TARGET): $(PRODUCT_OUT)/ramdisk.img
 	$(hide) mkdir -p $(PRODUCT_OUT)/boot
-	mkimage -A arm -O linux -T ramdisk -C gzip -d $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/boot/uInitrd
+	$(hide) mkimage -A $(TARGET_ARCH) -O linux -T ramdisk -C gzip -d $(INSTALLED_RAMDISK_TARGET) $@
 
-bootfiles: uinitrd
+# Pull this into the default 'droid' target
+droid: uinitrd
