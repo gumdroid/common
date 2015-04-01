119514d480d56915997b7d9f710fbc073da6b30b
Ash Charles <ashcharles@gmail.com>
2015-04-01 16:28:40 -0700

    Update uboot make procedure
    Ensure u-boot is only re-made when the source files change and include it in
    the default target.  As no other targets depend on the creation of MLO and
    u-boot (these aren't in the tarballs), don't worry about including it as an
    Android module---they can be built late---just as long as they're built.
    
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- uboot/Android.mk
+++ uboot/Android.mk
@@ -1,9 +1,10 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 ifdef UBOOT_CONFIG
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := uboot
+
 .phony: uboot clean-uboot
 
 UBOOT_SOURCE_DIR := $(abspath $(TOP)/u-boot)
@@ -11,25 +12,29 @@ UBOOT_SOURCE_DIR := $(abspath $(TOP)/u-boot)
 UBOOT_BARE_TOOLCHAIN := arm-eabi-
 UBOOT_BUILD_DIR := $(abspath $(PRODUCT_OUT)/obj/u-boot)
 UBOOT_OUTPUT_DIR := $(abspath $(PRODUCT_OUT)/boot)
+UBOOT_OUTPUT_FILES := $(UBOOT_OUTPUT_DIR)/MLO $(UBOOT_OUTPUT_DIR)/u-boot.img
 
 # build it and copy executables; avoid rebuilds if possible so don't reconfig. 
 uboot_not_configured := $(wildcard $(UBOOT_BUILD_DIR)/.config)
 
-uboot:
+uboot: $(UBOOT_OUTPUT_FILES)
+
+# Consider all files in the UBOOT_SOURCE_DIR to be prerequsites for this
+# target.  If any source files change, rebuild.
+$(UBOOT_OUTPUT_FILES): $(shell find $(UBOOT_SOURCE_DIR) -type f)
 	$(hide) mkdir -p $(UBOOT_BUILD_DIR)
 ifeq ($(strip $(uboot_not_configured)),)
 	$(hide) $(MAKE) O=$(UBOOT_BUILD_DIR) -C $(UBOOT_SOURCE_DIR) CROSS_COMPILE=$(UBOOT_BARE_TOOLCHAIN) $(UBOOT_CONFIG)_config
 endif
 	$(hide) $(MAKE) O=$(UBOOT_BUILD_DIR) -C $(UBOOT_SOURCE_DIR) CROSS_COMPILE=$(UBOOT_BARE_TOOLCHAIN)
 	$(hide) mkdir -p $(UBOOT_OUTPUT_DIR)
-	$(hide) cp $(UBOOT_BUILD_DIR)/MLO $(UBOOT_OUTPUT_DIR)/
-	$(hide) cp $(UBOOT_BUILD_DIR)/u-boot.img $(UBOOT_OUTPUT_DIR)/
+	$(hide) cp $(UBOOT_BUILD_DIR)/MLO $(UBOOT_BUILD_DIR)/u-boot.img $(UBOOT_OUTPUT_DIR)
 
 clean-uboot:
 	$(hide) -$(MAKE) O=$(UBOOT_BUILD_DIR) -C $(UBOOT_SOURCE_DIR) CROSS_COMPILE=$(UBOOT_BARE_TOOLCHAIN) distclean
-	$(hide) -rm $(UBOOT_OUTPUT_DIR)/MLO $(UBOOT_OUTPUT_DIR)/u-boot.img
+	$(hide) -rm $(UBOOT_OUTPUT_FILES)
 
-# Our device wants u-boot, add this to the bootfiles target
-bootfiles: uboot
+# Our device wants u-boot, so add this to the default 'droid' target
+droid: uboot
 
 endif # UBOOT_CONFIG
