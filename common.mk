a8fa5005bb6712e1529ddf20749556c1ccb7be12
Ash Charles <ashcharles@gmail.com>
2015-04-08 12:09:37 -0700

    Migrate gumstix common content from device-specific repositories
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- common.mk
+++ common.mk
@@ -1,6 +1,39 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+## Setup *distro*-type choices for Gumstix
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+DEVICE_PACKAGE_OVERLAYS := device/gumstix/common/overlay
+
+
+# Precise garbage collection
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Don't add visual fault overlays to our eng build
+PRODUCT_PROPERTY_OVERRIDES += \
+        persist.sys.strictmode.visual=0 \
+        persist.sys.strictmode.disable=1
+
+# System Utilities
+PRODUCT_PACKAGES += \
+        fs_get_stats \
+        dhcpcd.conf \
+        make_ext4fs
+
+# Audio utils
+PRODUCT_PACKAGES += \
+        tinycap \
+        tinymix \
+        tinyplay
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        MagicSmokeWallpapers \
+        VisualizationWallpapers
+
+# Gumstix-specific packages
 PRODUCT_PACKAGES += \
     mkandroidsd \
     devmem2 \
