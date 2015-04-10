eccd045786c893e6ce9725886a6c500dbc8b57ca
Ash Charles <ashcharles@gmail.com>
2015-04-10 15:08:51 -0700

    Support out-of-tree SGX build that rebuilds when needed
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- sgx/km/Android.mk
+++ sgx/km/Android.mk
@@ -1,29 +1,36 @@
-.phony: sgx-km clean-sgx-km
-SGX_KM_SRC_DIR := $(abspath $(TOP)/device/gumstix/common/sgx/km)
+.PHONY: sgx-km clean-sgx-km
+SGX_KM_SRC_DIR := $(abspath $(TOP)/hardware/ti/sgx)
 SGX_KM_BUILD_DIR := $(abspath $(TARGET_OUT_INTERMEDIATES)/SGX_OBJ)
+SGX_KM_OUT_DIR := $(TARGET_OUT)/etc
 
-# We can't use a yet-to-be determined /lib/modules path as a make
-# pre-requisite so install the modules to a known location in the output
-# file system and manually insmod them.
-SGX_SRC_FILES := $(SGX_KM_SRC_DIR)/echo.c $(SGX_KM_SRC_DIR)/Makefile
-SGX_BUILD_FILES := $(SGX_KM_BUILD_DIR)/echo.c $(SGX_KM_BUILD_DIR)/Makefile
-SGX_OUTPUT_FILE := $(TARGET_OUT)/etc/echo.ko
+SGX_KM_SRC_FILES := $(shell find $(SGX_KM_SRC_DIR) -type f)
+SGX_KM_BUILD_FILES := $(patsubst $(SGX_KM_SRC_DIR)/%,$(SGX_KM_BUILD_DIR)/%,$(SGX_KM_SRC_FILES))
+SGX_KM_BUILD_OUTPUT := $(SGX_KM_BUILD_DIR)/pvrsrvkm.ko $(SGX_KM_BUILD_DIR)/omaplfb.ko
+SGX_KM_OUTPUT_FILES := $(SGX_KM_OUT_DIR)/pvrsrvkm.ko $(SGX_KM_OUT_DIR)/omaplfb.ko
+
+# Hook onto the 'canary' target
+# use an intermediate as we've got multiple build outputs
+# http://stackoverflow.com/questions/2973445/gnu-makefile-rule-generating-a-few-targets-from-a-single-source-file
+$(TARGET_OUT)/etc/canary sgx-km $(SGX_KM_OUTPUT_FILES): sgx-km-output.intermediate
+
+.INTERMEDIATE: sgx-km-output.intermediate
+# build and copy output
+sgx-km-output.intermediate: $(SGX_KM_BUILD_FILES) $(INSTALLED_KERNEL_TARGET) $(ACP) | $(SGX_KM_OUT_DIR)
+	$(hide) rm -f $(SGX_KM_BUILD_OUTPUT)
+	$(hide) unset OUT && make -C $(SGX_KM_BUILD_DIR) -f Makefile.KM.Android buildkernel OMAPES=$(OMAPES) GRAPHICS_INSTALL_DIR=$(SGX_KM_BUILD_DIR) ANDROID_ROOT_DIR=$(abspath $(TOP)) KERNEL_INSTALL_DIR=$(KERNEL_BUILD_DIR) KERNELSRC=$(KERNEL_SOURCE_DIR)
+	$(hide) $(ACP) $(SGX_KM_BUILD_OUTPUT) $(SGX_KM_OUT_DIR)
+
+$(SGX_KM_OUT_DIR):
+	$(hide) mkdir -p $@
 
 # It is not clear how to build an external kernel module where the build output
 # is in a different direction from the source.  As such, copy the source files
 # to the desired build directory and build in-place there...sigh!
-sgx-km $(SGX_OUTPUT_FILE): $(SGX_BUILD_FILES) $(linux) | $(SGX_KM_BUILD_DIR)
-	$(hide) $(MAKE) -C $(KERNEL_BUILD_DIR) M=$(SGX_KM_BUILD_DIR) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(KERNEL_TOOLCHAIN_PREFIX)
-	$(hide) $(ACP) $(SGX_KM_BUILD_DIR)/echo.ko $(SGX_OUTPUT_FILE)
-
-$(SGX_BUILD_FILES): $(SGX_SRC_FILES) | $(SGX_KM_BUILD_DIR)
-	$(hide) $(ACP) $(SGX_SRC_FILES) $(SGX_KM_BUILD_DIR)
+$(SGX_KM_BUILD_DIR)/%: $(SGX_KM_SRC_DIR)/% $(ACP)
+	$(hide) mkdir -p $(@D)
+	$(hide) $(ACP) $< $@
 
 clean-sgx-km:
-	$(hide) -rm -rf $(SGX_OUTPUT_FILE)
-	$(hide) -$(MAKE) O=$(KERNEL_BUILD_DIR) -C $(KERNEL_SOURCE_DIR) M=$(SGX_KM_SRC_DIR) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(KERNEL_TOOLCHAIN_PREFIX) clean
+	$(hide) -rm -rf $(SGX_KM_OUTPUT_FILES)
+	$(hide) -rm -rf $(SGX_KM_BUILD_DIR)
 
-$(SGX_KM_BUILD_DIR):
-	$(hide) mkdir -p $@
-
-$(TARGET_OUT)/etc/canary: $(SGX_OUTPUT_FILE)
